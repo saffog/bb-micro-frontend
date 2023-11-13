80be366e75fe75ef1b3c32678e0d288bc52d1a7e
hxadev <alfonso_h_x@hotmail.com>
2023-11-13 17:07:45 -0600

    Updating Landing Design

--- HostApp/src/molecules/login-layout/index.tsx
+++ HostApp/src/molecules/login-layout/index.tsx
@@ -3,26 +3,26 @@ import { Button } from '../../atoms/button';
 import ButtonProps from '../../interfaces/button-props.interface';
 import styles from './index.module.css';
 
-interface Props{
-    title:string;
-    bodyContent:string|Element;
-    buttonProps?:ButtonProps;
+interface Props {
+    title: string;
+    bodyContent: string | Element;
+    buttonProps?: Array<ButtonProps>;
     backgroundColor?: string;
 
 }
-export const LoginLayout = ({title,bodyContent, buttonProps, backgroundColor}:Props) => {
-  return (
-    <div className={styles.container} style={{backgroundColor}}>
-        <div className={styles.header} style={{flex:1}}>
-            <h1>{title}</h1>
+export const LoginLayout = ({ title, bodyContent, buttonProps, backgroundColor }: Props) => {
+    return (
+        <div className={styles.container}>
+            <div className={styles.header} style={{ flex: 1 }}>
+                <h1>{title}</h1>
+            </div>
+            <div className={styles.body} style={{ flex: 1 }}>
+                <span>{bodyContent}</span>
+            </div>
+            <div className={styles.actions} style={{ flex: 1 }}>
+                {buttonProps?.map(btn => <Button {...btn} />)}
+            </div>
+
         </div>
-        <div className={styles.body} style={{flex:1}}>
-            <span>{bodyContent}</span>
-        </div>
-        <div className={styles.actions} style={{flex:1}}>
-            <Button {...buttonProps}/>
-        </div>
-        
-    </div>
-  )
+    )
 }
