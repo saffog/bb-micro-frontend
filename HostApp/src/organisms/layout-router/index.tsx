8d9395cb031e27e3aef277a2986823d8a69275ce
Freddy Rodriguez Andrade <freddy.rodriguezandrade@externos-cl.cencosud.com>
2023-11-17 12:46:19 -0500

    chore: update hostname

--- HostApp/src/organisms/layout-router/index.tsx
+++ HostApp/src/organisms/layout-router/index.tsx
@@ -1,13 +1,43 @@
-import React from 'react'
-import { personalLoginAppPrefix, companyLoginAppPrefix } from '../../constants/routes.constant'
-import { Link, Outlet } from 'react-router-dom'
+import React, {useEffect} from 'react'
+import {
+  personalLoginAppPrefix,
+  companyLoginAppPrefix,
+  personalDashboardAppPrefix, internetBankingMenuAppPrefix
+} from '../../constants/routes.constant'
+import {Link, Outlet, useNavigate} from 'react-router-dom'
 import { NavigationBar } from '../navigation-bar'
 
 export const LayoutRouter = () => {
+  const navigate=useNavigate();
+
+  useEffect(() => {
+    window.addEventListener('[PersonalLoginApp] login', (event) => {
+      navigate(`/${internetBankingMenuAppPrefix}`)
+      console.log('EVENT LISTENER', event);
+    });
+
+    window.addEventListener('[PersonalLoginApp] signup', (event) => {
+      navigate(`/${personalDashboardAppPrefix}`)
+      console.log('EVENT LISTENER', event);
+    });
+
+    window.addEventListener('[PersonalLoginApp] forgot-password', (event) => {
+      navigate(`/${personalDashboardAppPrefix}`)
+      console.log('EVENT LISTENER', event);
+    });
+
+    return () => {
+      window.removeEventListener('[PersonalLoginApp] login', () => null);
+      window.removeEventListener('[PersonalLoginApp] signup', () => null);
+      window.removeEventListener('[PersonalLoginApp] forgot-password', () => null);
+
+    }
+  }, []);
+
   return (
     <>
-    <NavigationBar />
-    <Outlet />
+      <NavigationBar />
+      <Outlet />
     </>
   )
 }
--- HostApp/src/routing/Router.tsx
+++ HostApp/src/routing/Router.tsx
@@ -5,7 +5,7 @@ import {
 } from "react-router-dom";
 import { routes } from "./routes";
 
-//Create a Broser Router with the routes declared in the routes component
+//Create a Broser Routes with the routes declared in the routes component
 const browserRouter = createBrowserRouter(routes);
 
 export function Router() {
