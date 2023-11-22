6f1a4851f12956a5da145c5b3b16a4764eebdb8f
Santiago Vilchis Reyes <svilchis@baufest.com>
2023-11-22 16:48:18 -0600

    Design changes in the dashboard screen

--- mf-personal-dashboard/src/components/organisms/graphic.tsx
+++ mf-personal-dashboard/src/components/organisms/graphic.tsx
@@ -5,11 +5,16 @@ import { data, options, dataChart, optionsChart, dataRadar } from "../data";
 export const Graphic = () => {
   
   return (
-    <div className="text-center bg-blue-500 rounded-lg shadow-lg ml-2 p-4 mb-2" >
+    <div className="text-center mx-2 p-4 mb-2" >
         <h1 className="border-2 border-black rounded-lg mb-2">Información sobre la cuenta</h1>
-        <div className='flex-row'>
-          <Line className="mx-4 bg-white mt-2" options={options} data={data}></Line>
-          <Line className="mx-4 bg-white mt-2" options={optionsChart} data={dataChart}></Line>
+        <div className='flex flex-row'>
+          
+          <div className='basis-1/2' >
+            <Line className="mx-4 bg-white" options={options} data={data}></Line>
+          </div>
+          <div className='basis-1/2'>
+            <Line className="mx-4 bg-white" options={optionsChart} data={dataChart}></Line>
+          </div>
           
         </div>
     </div>
