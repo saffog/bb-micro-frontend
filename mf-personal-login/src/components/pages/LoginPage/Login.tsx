0852ca74a14de722de0aed12ca5e77c456f00fdc
Freddy Rodriguez Andrade <freddy.rodriguezandrade@externos-cl.cencosud.com>
2023-12-05 10:43:14 -0500

    fix: improvement side-nav styles and functionality

--- mf-personal-login/src/components/pages/LoginPage/Login.tsx
+++ mf-personal-login/src/components/pages/LoginPage/Login.tsx
@@ -4,21 +4,21 @@ import {Link} from 'react-router-dom';
 import { FaUserLock } from 'react-icons/fa6';
 
 import ButtonForm from '../../atoms/Button';
-import CheckboxForm from '../../atoms/CheckBox';
+// import CheckboxForm from '../../atoms/CheckBox';
 import InputLabel from '../../molecules/InputLabel';
 import Card from '../../molecules/Card';
 import usePost from '../../../hooks/usePost';
 
 import styles from './Login.module.css';
-import Modal from './../../templates/ModalTemplate/index';
-import { contentTermsAndConditions } from './../../../constants/termsCond.constant';
+// import Modal from './../../templates/ModalTemplate/index';
+// import { contentTermsAndConditions } from './../../../constants/termsCond.constant';
 
 const Login = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [saveData, setSaveData] = useState(false);
+  // const [saveData, setSaveData] = useState(false);
   const {callPost, error} = usePost('/login-person');
-  const [modalOpen, setModalOpen] = useState(false);
+  // const [modalOpen, setModalOpen] = useState(false);
 
   const handleEmailChange = (e: ChangeEvent<HTMLInputElement>) => {
     setEmail(e.target.value);
@@ -27,14 +27,14 @@ const Login = () => {
   const handlePasswordChange = (e: ChangeEvent<HTMLInputElement>) => {
     setPassword(e.target.value);
   };
-
-  const handleOnClose = () => {
-    setModalOpen(false);
-  };
-
-  const onClickTermsAndConditions=()=>{
-    setModalOpen(true);
-  }
+  //
+  // const handleOnClose = () => {
+  //   setModalOpen(false);
+  // };
+  //
+  // const onClickTermsAndConditions=()=>{
+  //   setModalOpen(true);
+  // }
 
   const handleSubmit = async (e: FormEvent) => {
     e.preventDefault();
@@ -69,15 +69,15 @@ const Login = () => {
               value={password}
               onChange={handlePasswordChange}
             />
-            <CheckboxForm
-              name='agreeTerms'
-              checked={saveData}
-              onChange={() => setSaveData(!saveData)}
-              callbackMap={new Map().set('agreeTerms', onClickTermsAndConditions)}
-              title='¿Recuerdame?'
-              className={styles.saveData}
-              required={false}
-            />
+            {/*<CheckboxForm*/}
+            {/*  name='agreeTerms'*/}
+            {/*  checked={saveData}*/}
+            {/*  onChange={() => setSaveData(!saveData)}*/}
+            {/*  callbackMap={new Map().set('agreeTerms', onClickTermsAndConditions)}*/}
+            {/*  title='¿Recuerdame?'*/}
+            {/*  className={styles.saveData}*/}
+            {/*  required={false}*/}
+            {/*/>*/}
           </div>
           {!!error && <p className={styles.errorMessage}>Credenciales incorrectas. Inténtalo de nuevo.</p>}
           <div className={styles.actionForm}>
@@ -95,14 +95,14 @@ const Login = () => {
           <p>¿Eres nuevo usuario? <Link to="/signup">Registrate!</Link></p>
         </form>
       </Card>
-      {modalOpen && (
-        <Modal
-          title="Terminos y Condiciones"
-          content={contentTermsAndConditions}
-          onClose={handleOnClose}
-          buttonTitle="Volver"
-        />
-      )}
+      {/*{modalOpen && (*/}
+      {/*  <Modal*/}
+      {/*    title="Terminos y Condiciones"*/}
+      {/*    content={contentTermsAndConditions}*/}
+      {/*    onClose={handleOnClose}*/}
+      {/*    buttonTitle="Volver"*/}
+      {/*  />*/}
+      {/*)}*/}
     </div>
     
   );
