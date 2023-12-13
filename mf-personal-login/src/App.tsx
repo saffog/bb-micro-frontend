e924c33fdf5935e4fe407a9caf8d9cb2c220a29e
Freddy Rodriguez Andrade <freddy.rodriguezandrade@externos-cl.cencosud.com>
2023-12-13 10:28:37 -0500

    chore: advance about isolation for css classes

--- mf-personal-login/src/App.tsx
+++ mf-personal-login/src/App.tsx
@@ -1,14 +1,29 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import ReactDOM from 'react-dom/client';
-
-import {LocalRoutes} from './router/Routes';
 import {BrowserRouter} from 'react-router-dom';
+
 import {deferRender} from './mocks/mockingSetup';
+import {LocalRoutes} from './router/Routes';
+import {runStandalone} from '../styleLoader';
+
 import './index.css';
 
-const App = () => (
-  <LocalRoutes />
-);
+interface Props {
+  isStandalone?: boolean;
+}
+const App = ({isStandalone}: Props) => {
+
+  useEffect(() => {
+    // If we receive the isStandAlone prop we will initialize the style loader in standalone mode
+    if (isStandalone) {
+      runStandalone();
+    }
+  }, [isStandalone]);
+
+  return (
+    <LocalRoutes/>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById('appPersonalLogin-mfe')!);
 
@@ -16,7 +31,7 @@ deferRender().then(() => {
   root.render(
     <React.StrictMode>
       <BrowserRouter>
-        <App />
+        <App isStandalone />
       </BrowserRouter>
     </React.StrictMode>
   );
